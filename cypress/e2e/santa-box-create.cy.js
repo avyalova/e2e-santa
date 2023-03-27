7bea7060e8f9a90a39530d792d73641b183663d2
Anna <anna_vyalova@yahoo.com>
2023-03-27 09:20:01 +0200

    Modify HW-18-2

--- cypress/e2e/santa-box-create.cy.js
+++ cypress/e2e/santa-box-create.cy.js
@@ -81,7 +81,7 @@ describe("user can create a box and run it", () => {
   it("start Loteri", () => {
     cy.startLoteri()
     cy.contains("Жеребьевка проведена").should("exist")
-    //cy.clearAllCookies()
+    cy.clearAllCookies()
   })
 
   it("user1 checks notification about loteri", () => {
@@ -104,18 +104,34 @@ describe("user can create a box and run it", () => {
     cy.clearCookies()
   })
 
-  it("user3 checks notification about loteri", () => {
-    cy.visit("/login")
-    cy.login(users.user3.email, users.user3.password)
-    cy.get(generalElements.notificationButton).click()
-    cy.contains(
-      `У тебя появился подопечный в коробке "${newBoxName}". Скорее переходи по кнопке, чтобы узнать кто это!`
-    ).should("exist")
-    cy.clearCookies()
-  })
+  // it("user3 checks notification about loteri", () => {
+  //   cy.visit("/login")
+  //   cy.login(users.user3.email, users.user3.password)
+  //   cy.get(generalElements.notificationButton).click()
+  //   cy.contains(
+  //     `У тебя появился подопечный в коробке "${newBoxName}". Скорее переходи по кнопке, чтобы узнать кто это!`
+  //   ).should("exist")
+  //   cy.clearCookies()
+  // })
 
-  after("delete box", () => {
-    let Api = Cypress.config("baseUrl") + "/api/box/" + idBox
-    cy.ApiDeleteBox(Api)
+  Cypress.Commands.add("ApiDeleteBox", (Api) => {
+    cy.request({
+      url: "/api/login",
+      failOnStatusCode: false,
+      method: "POST",
+      body: {
+        email: "washingtonqwerty@gmail.com",
+        password: "123456",
+      },
+    })
+
+    cy.request({
+      method: "DELETE",
+      // headers: {
+      //   Cookie:
+      //     "jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMwMDEzNzMsImlhdCI6MTY3OTg1NjMxOCwiZXhwIjoxNjc5ODU5OTE4fQ.zw7Ez_LMfpT-4GcvQPyuVkDSXvEEvHwrGw7QMHitUXI; _ym_isad=2; _ohmybid_cmf=2; pm=6roxbbvlalo1wki2kqc48fhs9sa9ejgtkdx; _ym_d=1679856012; _ym_uid=1679344497311567266; adtech_uid=69e67548-3193-4473-a12a-a81bddfe0c60%3Alpitko.ru; top100_id=t1.7627570.2054377752.1679855936257; t3_sid_7627570=s1.717573348.1679855936258.1679856112717.1.7; last_visit=1679848912134%3A%3A1679856112134",
+      // },
+      url: "/api/box/" + idBox,
+    })
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -61,24 +61,3 @@ Cypress.Commands.add("startLoteri", () => {
   cy.get(loteriPage.submitButton).click({ force: true })
   cy.get(loteriPage.confirmButton).click({ force: true })
 })
-
-Cypress.Commands.add("ApiDeleteBox", (Api) => {
-  cy.request({
-    url: "/api/login",
-    failOnStatusCode: false,
-    method: "POST",
-    body: {
-      email: "washingtonqwerty@gmail.com",
-      password: "123456",
-    },
-  })
-
-  cy.request({
-    method: "DELETE",
-    // headers: {
-    //   Cookie:
-    //     "jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMwMDEzNzMsImlhdCI6MTY3OTg1NjMxOCwiZXhwIjoxNjc5ODU5OTE4fQ.zw7Ez_LMfpT-4GcvQPyuVkDSXvEEvHwrGw7QMHitUXI; _ym_isad=2; _ohmybid_cmf=2; pm=6roxbbvlalo1wki2kqc48fhs9sa9ejgtkdx; _ym_d=1679856012; _ym_uid=1679344497311567266; adtech_uid=69e67548-3193-4473-a12a-a81bddfe0c60%3Alpitko.ru; top100_id=t1.7627570.2054377752.1679855936257; t3_sid_7627570=s1.717573348.1679855936258.1679856112717.1.7; last_visit=1679848912134%3A%3A1679856112134",
-    // },
-    url: Api,
-  })
-})
